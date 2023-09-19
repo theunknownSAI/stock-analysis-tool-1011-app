4284da3084eba6d07d8a0e2b2e64fea6d2173fd5
Venkata Sai Krishna Nama <39611354+theunknownSAI@users.noreply.github.com>
2023-09-19 22:11:47 +0530

    config updated

--- next.config.js
+++ next.config.js
@@ -1,20 +1,2 @@
-const path = require("path");
-
 module.exports = {
-  trailingSlash: true,
-  exportPathMap: async () => {
-    return {
-      "/home": { page: "/components/Home" },
-      "/simulation": { page: "/components/Home" },
-      "/sectors": { page: "/components/Home" },
-      "/revenue": { page: "/components/Home" },
-      "/performance": { page: "/components/Home" },
-      "/comparision": { page: "/components/Home" },
-      "/sp500": { page: "/components/Home" },
-      "/login": { page: "/components/Home" },
-      "/about": { page: "/components/Home" },
-      "/top/:num/:type": { page: "/components/Home" },
-      "/companydetails/:name": { page: "/components/Home" },
-    };
-  },
 };
