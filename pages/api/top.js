80369549f447d2770e53c16020ff2facc8a4d772
Venkata Sai Krishna Nama <39611354+theunknownSAI@users.noreply.github.com>
2023-09-27 11:08:13 +0530

    api updated

--- pages/api/top.js
+++ pages/api/top.js
@@ -4,42 +4,36 @@ export const config = {
     externalResolver: true,
   },
 };
-export default (req, res, next) => {
+export default async (req, res, next) => {
   try {
     const type = req.query["type"];
     const num = parseInt(req.query["num"]);
-    // res.send([type, num]);
     const topurl =
       "https://raw.githubusercontent.com/saikr789/stock-analysis-tool-1011-data/master/Data/Top/type_180.csv";
-    axios
+    await axios
       .get(topurl.replace("type", type))
-      .then((s) => {
-        if (s.status === 200) {
+      .then((response) => {
+        if (response.status === 200) {
           let topdetails = [];
-          let rows = s.data.split("\n");
+          let rows = response.data.split("\n");
           const header = rows[0].split(",");
           const companyindex = header.indexOf("company");
           for (let i = 1; i < num + 1; i++) {
             const row = rows[i];
             const cols = row.split(",");
-            // var result = cols.reduce(function (result, field, index) {
-            //   result[header[index].replace(/(\r\n|\n|\r)/gm, "")] =
-            //     field.replace(/(\r\n|\n|\r)/gm, "");
-            //   return result;
-            // }, {});
             topdetails.push(cols[companyindex]);
           }
-          res.send(topdetails);
+          res.status(response.status).send({ details: topdetails, message: "Success" });
         } else {
-          res.status(404).send({ error: "error" });
+          res.status(response.status).send({ details: [], message: "Error" });
         }
       })
       .catch((error) => {
         console.log(error);
-        res.status(404).send({ error: "error" });
+        res.status(500).send({ details: [], message: "Error" });
       });
   } catch (error) {
     console.log(error);
-    res.status(404).send({ error: "error" });
+    res.status(500).send({ details: [], message: "Error" });
   }
 };
