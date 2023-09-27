21be12cb2cb8dde17189b35a7785e8d63123f8ca
Venkata Sai Krishna Nama <39611354+theunknownSAI@users.noreply.github.com>
2023-09-27 11:08:25 +0530

    updated to functional component

--- pages/components/Top.jsx
+++ pages/components/Top.jsx
@@ -1,11 +1,9 @@
 import { Paper, Typography } from "@mui/material";
 import { createTheme, styled } from '@mui/material/styles';
 import axios from "axios";
-import moment from "moment";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import * as Loader from "react-loader-spinner";
-import { NavLink } from "react-router-dom";
-import { withRouter } from "../../utils/WithRouter";
+import { NavLink, useParams } from "react-router-dom";
 
 const PREFIX = "Top";
 const theme = createTheme();
@@ -35,96 +33,81 @@ const Root = styled('div')(({ theme }) => ({
   }
 }));
 
-class Top extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loading: false,
-      num: JSON.parse(localStorage.getItem("num")) || "",
-      type: JSON.parse(localStorage.getItem("type")) || "",
-      topCompanies: JSON.parse(localStorage.getItem("topCompanies")) || [],
-    };
-  }
+const Top = () => {
 
-  componentDidMount = () => {
-    const {router} = this.props;
-    const {params} = router;
-    const { num, type } = params; 
-    const prevnum = JSON.parse(localStorage.getItem("num"));
-    const prevtype = JSON.parse(localStorage.getItem("type"));
-    const topCompanies = JSON.parse(localStorage.getItem("topCompanies"));
-    // const curdate = moment().format("DD-MM-YYYY");
-    // const prevdate =
-    //   localStorage.getItem("date") == null
-    //     ? curdate
-    //     : localStorage.getItem("date");
-    if (
-      prevtype === type &&
-      prevnum === num &&
-      topCompanies != null 
-      // &&  prevdate == curdate
-    ) {
-      return;
-    }
+  const [loading, setLoading] = useState(false);
+  const [num, setNum] = useState("");
+  const [type, setType] = useState("");
 
-    this.setState({ num: num, type: type, loading: true }, () => {
-      localStorage.setItem("num", JSON.stringify(this.state.num));
-      localStorage.setItem("type", JSON.stringify(this.state.type));
-    });
-    axios
-      .get("/api/top?type=" + type + "&" + "num=" + num)
-      .then((s) => {
-        if (s.status === 200) {
-          let topCompanies = s.data;
-          this.setState({ topCompanies: topCompanies, loading: false }, () => {
-            localStorage.setItem(
-              "topCompanies",
-              JSON.stringify(this.state.topCompanies)
-            );
-          });
+  const [topCompanies, setTopCompanies] = useState([]);
+
+  const params = useParams();
+  const paramNum = params.num;
+  const paramType = params.type;
+
+  useEffect(() => {
+    setLoading(true);
+    setNum(paramNum);
+    setType(paramType);
+    getTopCompanies();
+  }, [paramNum, paramType]);
+
+  const getTopCompanies = async () => {
+    await axios
+      .get("/api/top?type=" + paramType + "&" + "num=" + paramNum)
+      .then((response) => {
+        if (response.status === 200) {
+          let { data } = response;
+          let { details } = data;
+          let topCompanies = details;
+          console.log(topCompanies);
+          setLoading(false);
+          setTopCompanies(topCompanies);
+          localStorage.setItem("topCompanies", JSON.stringify(topCompanies));
         } else {
-          this.setState({ topCompanies: [], loading: false }, () => { });
+          setLoading(false);
+          setTopCompanies([]);
         }
       })
       .catch((e) => {
         console.log(e);
-        this.setState({ topCompanies: [], loading: false }, () => { });
+        setLoading(false);
+        setTopCompanies([]);
       });
-  };
-
-  render() {
-    return (
-      <Root className={classes.root}>
-        {this.state.loading ? (
-          <Loader.Audio sx={{ paddingLeft: "50%" }} />
-        ) : (
-          <div sx={{ backgroundColor: "inherit" }}>
-            <Paper elevation={0} className={classes.paper}>
-              <Typography variant="h4">
-                Top {this.state.num} Companies for{" "}
-                {this.state.type === "buy" ? "Investing" : "Trading"}
-              </Typography>
-            </Paper>
-            {this.state.topCompanies.map((company) => {
-              return (
-                <NavLink
-                  className={classes.navlink}
-                  key={company.toString()}
-                  to={{
-                    pathname: "/companydetails/" + company,
-                  }}
-                >
-                  <Paper elevation={0} className={classes.paper}>
-                    <Typography variant="h6">{company}</Typography>
-                  </Paper>
-                </NavLink>
-              );
-            })}
-          </div>
-        )}
-      </Root>
-    );
   }
+
+  console.log(topCompanies);
+  return (
+    <Root className={classes.root}>
+      {loading ? (
+        <Loader.Audio sx={{ paddingLeft: "50%" }} />
+      ) : (
+        <div sx={{ backgroundColor: "inherit" }}>
+          <Paper elevation={0} className={classes.paper}>
+            <Typography variant="h4">
+              Top {num} Companies for{" "}
+              {type === "buy" ? "Investing" : "Trading"}
+            </Typography>
+          </Paper>
+          {topCompanies.map((company) => {
+            return (
+              <NavLink
+                className={classes.navlink}
+                key={company.toString()}
+                to={{
+                  pathname: "/companydetails/" + company,
+                }}
+              >
+                <Paper elevation={0} className={classes.paper}>
+                  <Typography variant="h6">{company}</Typography>
+                </Paper>
+              </NavLink>
+            );
+          })}
+        </div>
+      )}
+    </Root>
+  );
 }
 
-export default withRouter(Top);
+export default Top;
